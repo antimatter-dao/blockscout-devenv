187435e037697806d9ff16a5261e21a755454879
0xe3b0c4 <0xe3b0c4@proton.me>
2022-08-23 20:21:32 +0800

    init commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+cd /app
+
+mix do deps.get, local.rebar --force, deps.compile
+cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode development; cd -
+cd apps/explorer && npm install; cd -
+mix phx.digest
+mix phx.server
